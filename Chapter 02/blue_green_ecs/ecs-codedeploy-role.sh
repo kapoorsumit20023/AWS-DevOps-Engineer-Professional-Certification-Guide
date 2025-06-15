36e25579ed42cd141e9557268a773868947e234b
Sumit Kapoor <sumitkapoor2008@.gmail.com>
2025-06-15 16:47:07 +0000

    added files

--- Chapter 02/blue_green_ecs/ecs-codedeploy-role.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-aws iam create-role --role-name ecsCodeDeployRole --assume-role-policy-document file://ecs-codedeploy-role-trust.json
-aws iam attach-role-policy --role-name ecsCodeDeployRole --policy-arn arn:aws:iam::aws:policy/AWSCodeDeployRoleForECS
-aws iam get-role --role-name ecsCodeDeployRole --query "Role.Arn" --output text
